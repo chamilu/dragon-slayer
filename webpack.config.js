1d4a9026b3f606175aa6d29f644797f5e48f2032
Chamil Udayanga <chamiludayanga28@gmail.com>
2018-07-30 09:45:09 +0800

    added file loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+              context: "./src"
+            }
+          }
+        ]
+      },
+      {
         test: /\.css$/,
         use: ["vue-style-loader", "css-loader"]
       },
